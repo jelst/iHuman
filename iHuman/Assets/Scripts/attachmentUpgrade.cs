e14b99763139def0640461c9c5553f57549f5052
jelst <jonathonelst15@gmail.com>
2018-08-31 20:24:29 -0400

    starter level and scripts

---
+++ iHuman/Assets/Scripts/attachmentUpgrade.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class attachmentUpgrade : MonoBehaviour {
+    public Transform player;
+    public GameObject upgradeObject;
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x < (positionX + 1) && player.position.x > (positionX - 1) && player.position.z < (positionZ + 1) && player.position.z > (positionZ - 1) && player.position.y < (positionY + 1) && player.position.y > (positionY - 1))
+        {
+            upgradeObject.GetComponent<BoxCollider>().enabled = true;
+            upgradeObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
---
+++ iHuman/Assets/Scripts/contactEnable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class contactEnable : MonoBehaviour {
+    public Transform player;
+    public GameObject enabledObject;
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x < (positionX + 1) && player.position.x > (positionX - 1) && player.position.z < (positionZ + 1) && player.position.z > (positionZ - 1) && player.position.y < (positionY + 1) && player.position.y > (positionY - 1))
+        {
+            enabledObject.SetActive(true);
+        }
+    }
+}
