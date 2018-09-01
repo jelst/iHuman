d47f003fe2f58ad9aa0faa6b0724db1b1f27eeaa
jelst <jonathonelst15@gmail.com>
2018-09-01 16:00:30 -0400

    level dev and glass

---
+++ iHuman/Assets/Scripts/shatterOnImpact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class shatterOnImpact : MonoBehaviour {
+
+    public GameObject shattered;
+    public int shatterForce;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > shatterForce)
+        {
+            GameObject.Instantiate(shattered, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
